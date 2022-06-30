1b4537aad5c499e221fffa34481e4c2af81d44cd
Mariam Conde <Condemriam@gmail.com>
2022-06-30 13:09:12 -0400

    frontend

---
+++ lifetracker-ui/src/components/AccessForbidden/AccessForbidden.jsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+
+
+export default function AccessForbidden() {
+    return ( <div className="access-forbid">
+        <div className="access-cont">
+            <div className="access-text">
+                <h1>Access Denied</h1>
+                <h2>Please login or sign up to access this content!</h2>
+            </div>
+        </div>
+    </div>);
+}
--- lifetracker-ui/src/components/App/App.jsx
+++ lifetracker-ui/src/components/App/App.jsx
@@ -9,6 +9,8 @@ import LandingPage from "components/LandingPage/LandingPage";
 import LoginPage from "components/LoginPage/LoginPage";
 import RegistrationPage from "components/RegistrationPage/RegistrationPage";
 import NotFound from "components/NotFound/NotFound";
+import AccessForbidden from "components/AccessForbidden/AccessForbidden";
+import ActivityPage from "components/ActivityPage/Activitypage";
 
 export default function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -29,8 +31,8 @@ export default function App() {
                 <Route path="/" element={<LandingPage />} />
                 <Route path="/login" element={<LoginPage isLoggedIn = {isLoggedIn} handleLoggedIn={handleLoggedIn}/>} />
                 <Route path="/register" element={<RegistrationPage isLoggedIn = {isLoggedIn}/>} />
-                {/* <Route path="/activity" element={<ActivityPage />} />
-                <Route path="/nutrition/*" element={<NutritionPage/>}/> */}
+                <Route path="/activity" element={<ActivityPage />} />
+                {/* <Route path="/nutrition/*" element={<NutritionPage/>}/> */}
                 <Route path="*" element={<NotFound/>}></Route>
               </Routes>
             </main>
